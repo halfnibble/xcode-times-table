180bfe025bbd9ca6b4ee02bf99bc35a23ed8e13b
Josh <josh@halfnibble.com>
2016-04-19 17:25:06 -0700

    Initial Times Table App.

--- Times Tables/ViewController.swift
+++ Times Tables/ViewController.swift
@@ -8,8 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var sliderValue: UISlider!
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        table.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 13
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        let multiplier: Int = Int(sliderValue.value)
+        
+        cell.textLabel!.text = "\(multiplier) x \(indexPath.row) = \(indexPath.row * multiplier)"
+        
+        cell.preservesSuperviewLayoutMargins = false
+        cell.separatorInset = UIEdgeInsetsZero
+        cell.layoutMargins = UIEdgeInsetsZero
+        
+        print("Cell indent: \(cell.separatorInset.left), \(cell.separatorInset.right), \(cell.separatorInset.top), \(cell.separatorInset.bottom)")
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
